8a45778f0e0601307943b2ae637ea1196b0b22bd
leo <18779775257@163.com>
2020-08-10 18:38:10 +0800

    add kylin route do

--- kylin-gateway/src/main/java/io/kyligence/kap/gateway/BootstrapServer.java
+++ kylin-gateway/src/main/java/io/kyligence/kap/gateway/BootstrapServer.java
@@ -16,18 +16,55 @@
 
 package io.kyligence.kap.gateway;
 
+import io.kyligence.kap.gateway.entity.KylinRouteRaw;
+import io.kyligence.kap.gateway.persistent.KylinRouteStore;
+import io.kyligence.kap.gateway.persistent.domain.KylinRouteDO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author zhiyu.zeng
  */
 
 @SpringBootApplication
-public class BootstrapServer {
+public class BootstrapServer implements CommandLineRunner {
+	@Autowired
+	KylinRouteStore kylinRouteStore;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootstrapServer.class, args);
 	}
 
+	@Bean
+	@ConfigurationProperties(prefix = "kylin.rest.connection")
+	public HttpComponentsClientHttpRequestFactory httpRequestFactory() {
+		return new HttpComponentsClientHttpRequestFactory();
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setRequestFactory(httpRequestFactory());
+		return restTemplate;
+	}
+
+	public void testGetKylinRouteRow() {
+		KylinRouteDO kylinRouteDO = new KylinRouteDO(1, "[\"10.1.2.56:7070\",\"10.1.2.166:7070\",\"10.1.2.167:7070\"]",
+				"project1", "common_query_1", "CUBE", "1c4b3f35-21f9-44b2-a2de-ae2d5a94189f");
+		KylinRouteRaw kylinRouteRaw = new KylinRouteRaw(kylinRouteDO);
+		System.out.println(kylinRouteRaw.getBackends());
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		testGetKylinRouteRow();
+	}
 }
---
+++ kylin-gateway/src/main/java/io/kyligence/kap/gateway/persistent/domain/KylinRouteDO.java
@@ -0,0 +1,35 @@
+package io.kyligence.kap.gateway.persistent.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+/**
+ * @author zhiyu.zeng
+ */
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KylinRouteDO {
+	private long id;
+	private String backends;
+	private String project;
+	private String resourceGroup;
+	private String type;
+	private String cluster;
+
+	@Override
+	public String toString() {
+		return "KylinRouteDO{" +
+				"id=" + id +
+				", backends='" + backends + '\'' +
+				", project='" + project + '\'' +
+				", resourceGroup='" + resourceGroup + '\'' +
+				", type='" + type + '\'' +
+				", cluster='" + cluster + '\'' +
+				'}';
+	}
+}
